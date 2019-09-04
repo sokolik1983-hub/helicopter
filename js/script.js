5f3e49793aacfb428f3305dd8dcfc3394f65863b
sokolik1983-hub <lionsbet@outlook.com>
2019-09-05 00:25:13 +0300

    add main section

--- js/script.js
+++ js/script.js
@@ -19,3 +19,55 @@ $('.gallery__link--invisible').on('click', function(){
     $('.gallery__invisible').css('display','none');
     $('.gallery__link').css('display','block');
 });
+$('.aboutme__link').on('click', function(){
+    $('.aboutme__invisible').css('display','block');
+    $('.aboutme__link').css('display','none');
+});
+$('.aboutme__link--invisible').on('click', function(){
+    $('.aboutme__invisible').css('display','none');
+    $('.aboutme__link').css('display','block');
+});
+$('.aboutme__link1').on('click', function(){
+    $('.aboutme__invisible1').css('display','block');
+    $('.aboutme__link1').css('display','none');
+});
+$('.aboutme__link--invisible1').on('click', function(){
+    $('.aboutme__invisible1').css('display','none');
+    $('.aboutme__link1').css('display','block');
+});
+
+$('.aboutme__profession-link').on('click', function(){
+    $('.aboutme__photo--invisible').css('display','block');
+    $('.aboutme__profession-link').css('display','none');
+});
+$('.aboutme__profession-close').on('click', function(){
+    $('.aboutme__photo--invisible').css('display','none');
+    $('.aboutme__profession-link').css('display','block');
+});
+
+$('.feedback__link').on('click', function(){
+    $('.feedback__invisible').css('display','block');
+    $('.feedback__link').css('display','none');
+});
+$('.feedback__link--close').on('click', function(){
+    $('.feedback__invisible').css('display','none');
+    $('.feedback__link').css('display','block');
+});
+
+
+
+
+
+
+
+
+
+
+$('.map__link').on('click', function(){
+    $('.map__invisible').css('display','block');
+    $('.map__link').css('display','none');
+});
+$('.map__link--invisible').on('click', function(){
+    $('.map__invisible').css('display','none');
+    $('.map__link').css('display','block');
+});
